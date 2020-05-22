a9ddf7b1785baf63986f11c1646d3c8c794abd42
daovanduc <duc07092000@gmail.com>
2020-05-22 18:34:22 +0700

    update route and index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2 style="text-align: center">List Manager</h2>
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+            <div class="container">
+            <table class="table">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">STT</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>T001</td>
+                    <td>Duc</td>
+                    <td>07/09/2000</td>
+                    <td>duc@gmail.com</td>
+                    <td>
+                        <a class="btn btn-outline-primary btn-sm" href="">view</a>
+                        <a class="btn btn-outline-success btn-sm" href="">edit</a>
+                        <a class="btn btn-outline-danger btn-sm" href="">delete</a>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+            </div>
+        </div>
+        <div class="col-1"></div>
+    </div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('customer')->group(function () {
+    Route::get('index', function () {
+        return view('index');
+    });
+    Route::get('create', function () {
+
+    });
+    Route::post('store', function () {
+
+    });
+    Route::get('{id}/show', function () {
+
+    });
+    Route::get('{id}/edit', function () {
+
+    });
+    Route::patch('{id}/update', function () {
+
+    });
+    Route::delete('{id}/delete', function () {
+
+    });
+});
